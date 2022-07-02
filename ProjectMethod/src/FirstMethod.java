a3c9133fbe289616c19fbc82c1d4963f4f35a858
JAE <jack3787@naver.com>
2022-07-02 18:48:21 +0900

    New : method 기본기

---
+++ ProjectMethod/src/FirstMethod.java
@@ -0,0 +1,7 @@
+public class FirstMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello Method"); // System.out.println는 (?)를 출력해주는 method
+        System.out.println(Math.floor(1.1)); // Math는 (?.?)를 내림해서 출력해주는 method
+
+    }
+}
